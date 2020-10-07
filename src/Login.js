03f6a55f2cd570ebc941aa4937b33a6de9312767
MANPRIT SINGH <58713644+manprit619@users.noreply.github.com>
2020-10-07 03:21:32 +0200

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,69 +1,84 @@
-import React, { useState } from 'react';
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "./firebase";
 
 function Login() {
-    const history = useHistory();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const signIn = e => {
-        e.preventDefault();
+  const login = (event) => {
+    event.preventDefault(); // this stops the   refresh
 
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(auth => {
-                history.push('/')
-            })
-            .catch(error => alert(error.message))
-    }
+    // do the logic login..
 
-    const register = e => {
-        e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // logged in , redirect to homepage....
+        history.push("/");
+      })
+      .catch((e) => alert(e.message));
+  };
 
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then((auth) => {
-                // it successfully created a new user with email and password
-                if (auth) {
-                    history.push('/')
-                }
-            })
-            .catch(error => alert(error.message))
-    }
+  const register = (event) => {
+    event.preventDefault(); // this stops the   refresh
+    // do the register logic....
 
-    return (
-        <div className='login'>
-            <Link to='/'>
-                <img
-                    className="login__logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
-                />
-            </Link>
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          history.push("/");
+          // created a user and logged in....
+        }
+      })
+      .catch((e) => alert(e.message));
+  };
 
-            <div className='login__container'>
-                <h1>Sign-in</h1>
+  return (
+    <div className="login">
+      <Link>
+        <img
+          className="login_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
 
-                <form>
-                    <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+      <div className="login_container">
+        <h1>Sign in</h1>
 
-                    <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <form>
+          <h5>E-mail</h5>
+          <input
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={login} type="submit" className="login_signinbutton">
+            Sign In
+          </button>
+        </form>
 
-                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
-                </form>
-
-                <p>
-                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
-                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-                </p>
-
-                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
-            </div>
-        </div>
-    )
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button onClick={register} className="login_registrationbutton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
