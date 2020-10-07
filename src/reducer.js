07a6262a73d5687ec5b7e19a7b010361efbef30a
MANPRIT SINGH <58713644+manprit619@users.noreply.github.com>
2020-10-07 13:45:27 +0200

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -22,6 +22,12 @@ function reducer(state, action) {
   console.log(action);
 
   switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
     case "ADD_TO_BASKET":
       // logic for adding item to basket
       return {
