f3ebad8e5cc832d59509a95a06112fc485d159e0
MANPRIT SINGH <58713644+manprit619@users.noreply.github.com>
2020-10-07 13:44:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import React from "react";
+
+  
+import React, { useEffect } from "react";
 import Header from "./Header";
 import "./App.css";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function App() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  // use Effect << very powerful
+  // run a piece of code which runs based on a given condition
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+
+    return () => {
+      // any cleanup operations go in here
+      unsubscribe();
+    };
+  }, []);
+
+  //console.log("User IS >> ", user);
+
   return (
     <Router>
       <div className="app">
